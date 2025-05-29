c77fce459dffa6ce45b4080aa6377aa898df7fb9
Kwak yong jin <91864021+Frekion2002@users.noreply.github.com>
2025-05-29 09:29:38 +0900

    Update 알약.py

--- 4.목/알약.py
+++ 4.목/알약.py
@@ -1,3 +1,4 @@
+# DP 방식의 풀이 (괄호쌍 유효성 검사와 같은 느낌)
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10000)
@@ -24,4 +25,18 @@ while True:
     d = [[-1 for _ in range(31)] for _ in range(31)]
 
 for res in result:
+    print(res)
+
+# 수학적 풀이 (카탈란 수)
+import math
+
+def catalan_number(n):
+    return math.comb(2 * n, n) // (n + 1)
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+
+    res = catalan_number(n)
     print(res)
