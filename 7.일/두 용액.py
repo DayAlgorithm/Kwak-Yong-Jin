90001fc0264e1f7f487c5984299521b4f889ece1
Kwak yong jin <91864021+Frekion2002@users.noreply.github.com>
2025-08-26 20:25:01 +0900

    Create 두 용액.py

---
+++ 7.일/두 용액.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+
+l, r = 0, N-1
+best = float('inf')
+ans = (0, 0)
+
+while l < r:
+    s = arr[l] + arr[r]
+
+    if abs(s) < best:
+        best = abs(s)
+        ans = (arr[l], arr[r])
+
+    if s < 0:  
+        l += 1
+    elif s > 0:
+        r -= 1
+    else:      
+        break
+
+print(ans[0], ans[1])
